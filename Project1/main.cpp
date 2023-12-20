adeb5b935f250a07d8a43368dd4c8bd96b2ca18c
richardkg <richard.emc@gmail.com>
2023-12-20 12:51:24 +0000

    teste1

---
+++ Project1/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    cout << "hello world from project1" << endl;
+    int x {100};
+    cout <<"what is your favorite number" << endl;
+    cin >> x ;
+    cout << "your favorite number is " << x << endl;
+    return 0;
+
+}
---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    cout << "hello world from project1" << endl;
+    int x {100};
+    cout << x << endl;
+    return 0;
+
+}
