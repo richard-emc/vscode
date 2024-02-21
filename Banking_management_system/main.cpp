4d5c6078f61d943363b0592dff2edc876119c45a
richardkg <richard.emc@gmail.com>
2024-02-21 13:24:08 +0000

    Banking System done

--- Banking_management_system/main.cpp
+++ Banking_management_system/main.cpp
@@ -59,12 +59,9 @@ void account_query::read_rec()
         return;
     }
     cout<<"\n***Data from file***"<<endl;
-    while(!infile.eof())
+    while(infile.read(reinterpret_cast<char *>(this), sizeof(*this))) 
     {
-        if(infile.read(reinterpret_cast<char *>(this), sizeof(*this))>0)
-        {
-            show_data();
-        }
+    show_data();
     }
     infile.close();
 }
@@ -80,7 +77,7 @@ void account_query::seaarch_rec()
         return;
     }
     infile.seekg(0,ios::end);
-    int count = infile.tellg/sizeof(*this);
+    int count = infile.tellg()/sizeof(*this);
     cout<<"\n There are "<<count<<"record in the file";
     cout<<"\n Enter record number to search: ";
     cin>>n;
@@ -90,5 +87,104 @@ void account_query::seaarch_rec()
 
 void account_query::edit_rec()
 {
-    
+int n;
+fstream iofile;
+iofile.open("record.bank", ios::in|ios::binary);
+if(!iofile)
+ {
+    cout<<"Error in Opening! File Not Found!!"<<endl;
+    return;
+ }   
+iofile.seekg(0, ios::end);
+int count = iofile.tellg()/sizeof(*this);
+cout<<"\n There are "<<count<<"record in the file";
+cout<<"\n Enter record number to edit:";
+cin>>n;
+iofile.seekg((n-1)*sizeof(*this));
+iofile.read(reinterpret_cast<char*>(this), sizeof(*this));
+cout<<"Record"<<n<<"has following data"<<endl;
+show_data(); 
+iofile.close();
+iofile.open("record.bank", ios::out|ios::in|ios::binary);
+iofile.seekp((n-1)*sizeof(*this));
+cout<<"\n Enter data to modify:"<<endl;
+read_data();
+iofile.write(reinterpret_cast<char*>(this), sizeof(*this));
+}
+
+void account_query::delete_rec()
+{
+    int n;
+    ifstream infile;
+    infile.open("record.bank", ios::in|ios::binary);
+    if(!infile)
+    {
+        cout<<"Error in Opening! File Not Found!!"<<endl;
+        return;
+    }
+    infile.seekg(0, ios::end);
+    int count = infile.tellg()/sizeof(*this);
+    cout<<"\n There are "<<count<<"record in the file";
+    cout<<"\n Enter record number to delete:";
+    cin>>n;
+    fstream tmpfile;
+    tmpfile.open("tmpfile.bank", ios::out|ios::binary);
+    infile.seekg(0);
+    for(int i=0; i<count; i++)
+    {
+        infile.read(reinterpret_cast<char*>(this), sizeof(*this));
+        if(i==(n-1))
+            continue;
+        tmpfile.write(reinterpret_cast<char*>(this), sizeof(*this));
+    }
+    infile.close();
+    tmpfile.close();
+    remove("record.bank");
+    rename("tmpfile.bank", "record.bank");
+
+}
+
+int main()
+{
+    account_query A;
+    int choice;
+    cout<<"*** Account information System"<<endl;
+    while(true)
+    {
+        cout<<"Select one option below ";
+        cout<<"\n\t1-->Add record to file ";
+        cout<<"\n\t2-->Show record to file ";
+        cout<<"\n\t3-->Search record from file";
+        cout<<"\n\t4-->Update record";
+        cout<<"\n\t5-->Delete record";
+        cout<<"\n\t6-->Quit";
+        cout<<"\nEnter your choice; ";
+        cin>>choice;
+        switch(choice)
+        {
+            case 1:
+                A.write_rec();
+                break;
+            case 2:
+                A.read_rec();
+                break;
+            case 3:
+                A.seaarch_rec();
+                break;
+            case 4:
+                A.edit_rec();
+                break;
+            case 5:
+                A.delete_rec();
+                break;
+            case 6:
+                exit(0);
+                break;
+            default:
+                cout<<"\nEnter correct choice";
+                exit(0);
+        }
+    }
+    system("pause");
+    return 0;
 }
