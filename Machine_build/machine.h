0925965c042a7a32ffcbb3a558fa496e9c657173
richardkg <richard.emc@gmail.com>
2024-03-06 14:00:02 +0000

    ajuse machine.h

--- Machine_build/machine.h
+++ Machine_build/machine.h
@@ -1,18 +1,19 @@
 #ifndef _MACHINE_H_
 #define _MACHINE_H_
-#include <iostream>
-#include <vector>
-#include <fstream>
-#include <string>
 
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <iomanip>
+#include <string> // Add this line
 
 
 class Product{
-    string name;
+    std::string name;
     double price;
-    string category;
+    std::string category;
 
-    Product(string productName, double productPrice, string productCategory)
+    Product(std::string productName, double productPrice, std::string productCategory)
     {
         name = productName;
         price = productPrice;
@@ -25,12 +26,12 @@ class Product{
 
 class Machine{
     int machineID;
-    string name;
-    string projectNumber;
-    vector<Product> products;
+    std::string name;
+    std::string projectNumber;
+    std::vector<Product> products;
 
     public:
-    Machine(int id, string machineName, string projectNum,):
+    Machine(int id, std::string machineName, std::string projectNum):
     machineID(id), name(machineName), projectNumber(projectNum){}
 
 };
